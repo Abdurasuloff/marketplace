414b372364ca996abd775b215c3dfc7aaef1491e
Akbarjon <abdurasuloff2004@gmail.com>
2023-02-01 20:05:36 +0500

    add bulk update

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'main',
     'users',
     'products',
-    "debug_toolbar",
     
 ]
 
@@ -54,14 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+
 
 ROOT_URLCONF = 'core.urls'
 
--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('users/', include("users.urls")),
